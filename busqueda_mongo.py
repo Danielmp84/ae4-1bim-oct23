f938a28fcc52f04e6e46941954c8f6236750bcac
unknown <danielwmp1984@gmail.com>
2023-12-06 22:30:14 -0500

    se agrega archivo busqueda_mongo

---
+++ busqueda_mongo.py
@@ -0,0 +1,25 @@
+# Importar el MongoClient
+from base_datos import engine
+
+# Crear una instancia de MongoClient
+
+# Seleccionar la base de datos y la colección
+db = engine["Matriculas"]
+collection = db["registrosestudiantes"]
+
+# Consultar todos los registros en la colección
+resultados = collection.find()
+
+# Imprimir los resultados
+for resultado in resultados:
+    print(resultado)
+
+collection2 = db["registrosevehiculos"]
+
+# Consultar todos los registros en la colección
+resultados2 = collection2.find()
+
+# Imprimir los resultados
+for resultadovehiculos in resultados2:
+    print(resultadovehiculos)    
+    
